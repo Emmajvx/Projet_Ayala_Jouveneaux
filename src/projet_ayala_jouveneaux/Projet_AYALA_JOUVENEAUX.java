cd35f7d3c1ff49959225cfb55ec53cdc15edeab9
emma jouveneaux <emma.jouveneaux@epfedu.fr>
2023-11-13 15:25:44 +0100

    test 2

--- src/projet_ayala_jouveneaux/Projet_AYALA_JOUVENEAUX.java
+++ src/projet_ayala_jouveneaux/Projet_AYALA_JOUVENEAUX.java
@@ -14,7 +14,7 @@ public class Projet_AYALA_JOUVENEAUX {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        "test debut de projet";
+        //"test debut de projet";
     }
     
 }
